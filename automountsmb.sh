ecf0d43cae4f50ddb3f856ff78412de8acd32acd
Brendan <124538338+BDevop@users.noreply.github.com>
2024-03-24 12:18:49 -0400

    Added comments

--- automountsmb.sh
+++ automountsmb.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-#
+# Must ensure that cifs-utils & psmisc are installed
+# Must ensure that /mnt/pub directory exists.
+# Change username and password variables (do not use in production environment)
 mount -t cifs //<IP-Address-of-server>/directory-to-mount/ /mnt/pub -o username=<username>,password=<password>
